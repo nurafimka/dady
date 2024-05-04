e217285cd8e02f9e9bd4d2953d9d0da96cd4b3a3
nurafimka <efimova.anna2013@gmail.com>
2024-05-04 13:00:46 +0300

    Мой первый калькулятор!

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi nigga");
+        System.out.println("Добро пожаловать в калькулятор умножения, введи число!!!");
+        Double One = new Scanner(System.in).nextDouble();
+        System.out.println("Молодец, теперь введи второе число!");
+        Double Two = new Scanner(System.in).nextDouble();
+        System.out.println("НИЧОСИ ПОЛУЧИЛОСЬ:"+ One*Two);
     }
 }
